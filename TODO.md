2aa840c68f7b4f7c99f382f88f553523e2bd0bb1
Lucas Vianna <lucas@hover.to>
2023-11-17 10:49:03 -0500

    clean up

--- TODO.md
+++
@@ -1,5 +0,0 @@
-# ToDO
-
-- support files or directories only
-- top contributors
-- Create groups
--- Untitled-1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export SINCE=2022-01-01
-
-
-git log --pretty="%H" --author="Lucas Vianna" | while read commit_hash; do git show --oneline --name-only $commit_hash | tail -n+2; done | sort | uniq
